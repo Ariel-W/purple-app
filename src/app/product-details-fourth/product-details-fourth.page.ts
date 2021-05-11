d67c15f19c9ac00279aa688a691732d41b686e27
Ariel Weiss <ariel.weiss@gmail.com>
2021-05-11 11:43:24 +0300

    first push

---
+++ src/app/product-details-fourth/product-details-fourth.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-product-details-fourth',
+  templateUrl: './product-details-fourth.page.html',
+  styleUrls: ['./product-details-fourth.page.scss'],
+})
+export class ProductDetailsFourthPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
